acad357878f62e07a9a3a75b81c639ed63cf3651
Frikkie-G <114396865+Frikkie-G@users.noreply.github.com>
2023-09-06 18:26:44 +0200

    Chronic Medication Models

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chronic_Medication_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PrescriptionCalendarModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chronic_Medication_system.Models
+{
+    public class PrescriptionCalendarModel
+    {
+        [Key]
+        public int PatientID { get; set; }
+        [Required]
+        public string? PrescriptionName { get; set; }
+
+        [Required]
+        public DateTime prescription_end_date { get; set; }
+
+        [Required]
+        public string? eventDesc { get; set; }  
+    }
+}
